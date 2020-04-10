148c8092a2fa0e39071f71a5c43241c0898b57a1
Yey007 <utku_melemetci@yahoo.com>
2020-04-10 08:19:51 -0400

    Initial commit

---
+++ PaperAutoUpdate/src/main/java/io/github/yey007/PaperAutoUpdate/PaperAutoUpdate.java
@@ -0,0 +1,16 @@
+package io.github.yey007.PaperAutoUpdate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PaperAutoUpdate extends JavaPlugin
+{
+    @Override
+    public void onEnable() {
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        
+    }
+}
