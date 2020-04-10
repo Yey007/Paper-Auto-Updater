19878bedf31ba4f21f62f65f52a32124d590759b
Yey007 <utku_melemetci@yahoo.com>
2020-04-10 09:54:08 -0400

    Added some basic functions like onEnable and onCommand

--- PaperAutoUpdate/src/main/java/io/github/yey007/PaperAutoUpdate/PaperAutoUpdate.java
+++ PaperAutoUpdate/src/main/java/io/github/yey007/PaperAutoUpdate/PaperAutoUpdate.java
@@ -1,16 +1,46 @@
-package io.github.yey007.PaperAutoUpdate;
+package io.github.yey007.paperautoupdate;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PaperAutoUpdate extends JavaPlugin
+ public final class PaperAutoUpdate extends JavaPlugin
 {
     @Override
     public void onEnable() {
-        
+     
+        getLogger().info("Paper auto updater is no longer running.");
+
     }
-    
+
     @Override
     public void onDisable() {
-        
+        getLogger().info("Paper auto updater is no longer running.");
     }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+  
+        if(cmd.getName().equalsIgnoreCase("update")){
+ 
+            if(sender instanceof Player && sender.hasPermission("PaperAutoUpdate.update")) {
+
+                getLogger().info("Update has been called");
+                return true;
+
+            } else if (sender instanceof Player && !sender.hasPermission("PaperAutoUpdate.update")){
+
+                getLogger().info(this.getCommand("update").getPermissionMessage());
+                return false;
+
+            } else {
+
+                getLogger().info("Update has been called");
+                return true;
+
+            }
+        }
+		return false;
+    } 
 }
